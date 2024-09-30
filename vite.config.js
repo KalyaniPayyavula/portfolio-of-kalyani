f52ade13b5b4efa5ed729a5d42deb2bd02eb4f6e
Yugandhar Pathi <yugandharpathi@Yugandhars-MacBook-Pro.local>
2024-09-30 16:04:36 -0700

    Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-of-kalyani/', // Set this to match your GitHub repository name
 })
+
